250cdfe3b0e5dbb2b75b4fbdeb823b4c7750dd4d
MohamedMF2 <furious_fouad@yahoo.com>
2022-06-22 06:58:33 +0200

    initial commit

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,36 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string FirstName { get; set; }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + ',' + FirstName ;
+            }
+        }
+        public string EmailAddress { get; set;  }
+    }
+}
